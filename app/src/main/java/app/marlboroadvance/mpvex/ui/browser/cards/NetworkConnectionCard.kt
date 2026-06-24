6e9e1217332f976bda954791cebceebc308ca2fe
qznet <43272585+qznet@users.noreply.github.com>
2026-06-24 17:13:42 +0800

    Add files via upload

--- app/src/main/java/app/marlboroadvance/mpvex/ui/browser/cards/NetworkConnectionCard.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/browser/cards/NetworkConnectionCard.kt
@@ -27,7 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusable
+import androidx.compose.foundation.focusable
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import app.marlboroadvance.mpvex.domain.network.NetworkConnection
